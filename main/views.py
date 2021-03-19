fc6c47b087aeb5e7ac2e664894f24267a87e9f64
Lifanna3 <solano.lifan2@bk.ru>
2021-03-19 20:39:12 +0600

    asd

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from geopy.distance import vincenty
+# from geopy.distance import vincenty
 
 class HelloView(APIView):
     permission_classes = (IsAuthenticated,)
